7ca7328144d4f034471208689b2f41b1d730ed7f
25phamhaiyen <145901112+25phamhaiyen@users.noreply.github.com>
2024-10-10 21:51:23 +0700

    update

--- SourceCode/Main.cpp
+++ SourceCode/Main.cpp
@@ -1,3 +1,21 @@
+#include "BuilFunction.h"
 int main(){
-	
+	int choice;
+    do {
+        cout << "Chon chuc nang: " << endl;
+        cout << "1. ." << endl;
+        cout << "2. ." << endl;
+        cout << "3. ." << endl;
+        cout << "4. " << endl;
+        cout << "5. ." << endl;
+        cout << "6. ." << endl;
+        cout << "7. Thoat." << endl;
+        cout << "Enter your choice: ";
+        cin >> choice;
+
+        switch(choice){
+            case 1:
+            break;
+        }     
+    }while(1);
 }
