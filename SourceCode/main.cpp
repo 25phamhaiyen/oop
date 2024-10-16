e3a7352e8571cd16edfcfac474c4d8e4578c9925
Trần Chí Nguyên <153911883+Trannguyen08@users.noreply.github.com>
2024-10-16 17:35:52 +0700

    Update main.cpp

--- SourceCode/main.cpp
+++ SourceCode/main.cpp
@@ -27,25 +27,15 @@ int main(int argc, char** argv) {
 		si.sign_in(1);
 		cout << "\n---------------------------------------------\n";
 		system("cls");
-		cout << "---------------------MENU-----------------------\n\n";
-		cout << "1.Hien thi danh sach may bay.\n";
-		cout << "2.Hien thi danh sach chuyen bay.\n";
-		cout << "3.Hien thi danh sach thong tin nguoi dung.\n";
-		cout << "4.Hien thi danh sach luot dat ve.\n";
-		cout << "5.Hien thi danh sach nhan su.\n";
-		cout << "6.Hien thi danh sach Voucher.\n"
-		cout << "7.Them thong tin chuyen bay.\n";
-		cout << "8.Them Voucher.\n";
-		cout << "9.Hien thi tong doanh thu dat ve.\n";
-		cout << "T.Thoat.\n";
+		displayMenuManager();
 		char choice;
 		while( choice != 't' && choice != 'T' ){
 			do {
 				cout << "\nNhap lua chon cua ban:  ";
 				cin >> choice;
-				if( choice < '1' || choice > '9' )
+				if( choice < '1' || choice > '8' )
 					cout << "\nBan chi duoc nhap gia tri trong khoang tu 1 -> 9 hoac {t,T}. Vui long nhap lai.\n";
-			}while( choice < '1' || choice > '9' );
+			}while( choice < '1' || choice > '8' );
 			switch(choice){
 				case '1':
 					cout << endl;
@@ -79,12 +69,11 @@ int main(int argc, char** argv) {
 					cout << endl;
 					addVoucher(voucher);
 					break;
-				case '9':
-					cout << endl;
-					displayRevenue(history);
+				case 't':
+					cout << endl << "KET THUC\n";
 					break;
-				case 't','T':
-					cout << endl << "KET THUC" << endl;
+				case 'T':
+					cout << endl << "KET THUC\n";
 					break;
 			}
 		}	
@@ -114,6 +103,211 @@ int main(int argc, char** argv) {
 			si.sign_in(2);
 			cout << "\n---------------------------------------------\n";
 		}
+		char choice;
+		vector<pair<Flight, string>> passfly;
+		vector<pair<Passenger, string>> demoPass;
+		vector<pair<Passenger, string>> hisPass;
+		while( choice != '7' ){
+			system("cls");
+			displayMenuUser();
+			do {
+				cout << "Nhap lua chon cua ban:  ";
+				cin >> choice;
+				if( choice < '1' || choice > '7' )
+					cout << "\nBan chi duoc nhap trong khoang tu 1 -> 7. Vui long nhap lai\n\n";
+			}while( choice < '1' || choice > '7' );
+			system("cls");
+			switch( choice ){
+				case '1':
+					{
+						Passenger pas;
+						cin >> pas;
+						char yourChoice;
+						while( yourChoice != '8'){
+							cout << "\n---------------------MENU---------------------\n\n";
+							cout << "1. Hien thi danh sach chuyen bay.\n";
+							cout << "2. Tim kiem theo ngay bay.\n";
+							cout << "3. Tim kiem theo gia ve.\n";
+							cout << "4. Tim kiem theo hang bay.\n";
+							cout << "5. Tim kiem theo noi den.\n";
+							cout << "6. Tim kiem theo thoi gian.\n";
+							cout << "7. Sap xep tang dan gia ve.\n";
+							cout << "8. Thoat.\n";
+							cout << "\n----------------------------------------------\n\n";
+							cin.ignore();
+							do {
+								cout << "Nhap lua chon cua ban:  ";
+								cin >> yourChoice;
+								if( yourChoice < '1' || yourChoice > '8' )
+									cout << "\nBan chi duoc nhap trong khoang tu 1 -> 8. Vui long nhap lai\n\n";
+							}while( yourChoice < '1' || yourChoice > '8' );
+							system("cls");
+							switch( yourChoice ){
+								case '1':
+									cout << endl;
+									displayFlight(flight);
+									break;
+								case '2':
+									{
+										cout << endl;
+										Date date;
+										cout << "Nhap ngay bay ban muon tim:\n";
+										cin >> date;
+										findWithFlightDate(flight, date);
+										break;
+									}
+								case '3':
+									{
+										cout << endl;
+										double minTic, maxTic;
+										cout << "Nhap khoang gia ve muon tim: \n\n";
+										cout << "Tu:  "; cin >> minTic;
+										cout << "Den:  "; cin >> maxTic;
+										findWithTicketPrice(flight, minTic, maxTic, pas.getRank());
+										break;
+									}
+								case '4':
+									{
+										cout << endl;
+										char c;
+										string planeName;
+										cout << "Chon 1 trong cac hang bay:\n\n";
+										cout << "1. VietNam Airlines\n";
+										cout << "2. VietJet Air\n";
+										cout << "3. Bamboo Airways\n";
+										cout << "4. Jetstar Pacific\n\n";
+										do {
+											cout << "Nhap lua chon cua ban:  "; cin >> c;
+											if( c < '1' || c > '4' )
+												cout << "\nBan chi duoc nhap gia tri trong khaong 1 -> 4. Vui long nhap lai\n";
+										}while( c < '1' || c > '4' );
+										switch( c ){
+											case '1':
+												planeName = "VietNam Airlines";
+												break;
+											case '2':
+												planeName = "VietJet Air     ";
+												break;
+											case '3':
+												planeName = "Bamboo Airways  ";
+												break;
+											case '4':
+												planeName = "Jetstar Pacific ";
+												break;	
+										}
+										findWithPlaneName(flight, planeName);
+										break;
+									}
+								case '5':
+									{
+										cout << endl;
+										cin.ignore();
+										string destination;
+										cout << "Nhap noi den muon tim:  "; getline(cin, destination);
+										string space = "";
+										for( int i = 0 ; i < 9-destination.size() ; i++ ){
+											space += " ";
+										}
+										destination += space;
+										findWithDestination(flight, destination);
+										break;
+									}
+								case '6':
+									{
+										cout << endl;
+										Time sTime, eTime;
+										cout << "Nhap khoang thoi gian muon tim: \n\n";
+										cout << "Tu:  \n"; cin >> sTime;
+										do {
+											cout << "\nDen:  \n"; cin >> eTime;
+											if( eTime <= sTime )
+												cout << "\nThoi gian sau phai lon hon thoi gian truoc. Vui long nhap lai\n";
+										}while( eTime <= sTime );
+										findWithTime(flight, sTime, eTime);
+										break;
+									}
+								case '7':
+									{
+										cout << endl;
+										sortIncreaseTicketPrice(flight);
+										displayFlight(flight);
+										break;
+									}
+								case '8':
+									cout << endl << "KET THUC\n";
+									break;
+							}	
+							char ch;
+							cout << "\n Ban da tim duoc chuyen bay mong muon chua?\n";
+							cout << "1. Da tim duoc.\n";
+							cout << "2. Chua tim duoc.\n";
+							do {
+								cout << "\nNhap lua chon cua ban:  ";
+								cin >> ch;
+								if( ch != '1' && ch != '2' )
+									cout << "\nBan chi duoc chon 1 hoac 2. Vui logn chon lai\n";
+							}while( ch != '1' && ch != '2' );
+							cout << "------------------\n";
+							if( ch == '1' ){
+								string id, plane;
+								cout << "\nNhap id chuyen bay muon dat:  ";
+								cin >> id;
+								Flight f;
+								for( auto it : flight ){
+									if( it.first.getId() == id ){
+										f = it.first;
+										plane = it.second;
+										break;
+									}
+								}
+								if( pas.getRank() == "Thuong gia" ){
+									if( f.countVip() )
+										f.listVip();
+								}
+								else if( pas.getRank() == "Pho thong " ){
+									if( f.countPopular() )
+										f.listPopular();
+								}
+								cout << endl;
+								int pos;
+								do {
+									cout << "\nNhap vi tri ghe ban muon ngoi:  ";
+									cin >> pos;
+									if( pas.getRank() == "Thuong gia" && ( pos < 1 || pos > 15 ) ){
+										cout << "\nBan chi duoc chon cac vi tri ghe da liet ke o tren. Vui long chon lai.\n";
+									}
+									if( pas.getRank() == "Pho thong " && ( pos < 16 || pos > 50 ) ){
+										cout << "\nBan chi duoc chon cac vi tri ghe da liet ke o tren. Vui long chon lai.\n";
+									}
+								}while( ( pas.getRank() == "Thuong gia" && ( pos < 1 || pos > 15 ) ) || ( pas.getRank() == "Pho thong " && ( pos < 16 || pos > 50 ) ) );
+								pas.setPosition(pos);
+								demoPass.push_back(make_pair(pas,id));
+								hisPass.push_back(make_pair(pas,id));
+								bool check = true;
+								for( auto it : passfly ){
+									if( it.first.getId() == id ){
+										check = false;
+										break;
+									}			
+								}
+								if( check )
+									passfly.push_back(make_pair(f,plane));
+								yourChoice = '8';	
+							}
+							
+						}	
+					}
+				case '2':
+				case '3':
+				case '4':
+				case '5':
+				case '6':
+				case '7':
+					cout << endl << "KET THUC\n";
+					break;
+			}
+		}
+		
 	}
 	return 0;
 }
