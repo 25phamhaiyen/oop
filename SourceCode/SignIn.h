bcd16468a6478b1c7fbce40b3ef3a97a5f643012
Trần Chí Nguyên <153911883+Trannguyen08@users.noreply.github.com>
2024-10-18 22:13:02 +0700

    Update SignIn.h

--- SourceCode/SignIn.h
+++ SourceCode/SignIn.h
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<conio.h>
 #include<fstream>
+#include<sstream>
 using namespace std;
 class SignIn {
 	private:
@@ -15,5 +16,4 @@ class SignIn {
 	    void sign_up();
 	    string getUserName();
 	    string getPassword();
-		
 };
