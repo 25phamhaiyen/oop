d4fc7da55c16d390d20f8ad689b728b793c7fdcc
Trần Chí Nguyên <153911883+Trannguyen08@users.noreply.github.com>
2024-11-08 10:46:39 +0700

    Update date_time.h

--- SourceCode/date_time.h
+++ SourceCode/date_time.h
@@ -26,9 +26,9 @@ class Date {
 		Date( const Date &date );
 		// hàm nhập ngày
 		friend void inputDay( int &day, int maxDay );
-		// hàm nhập ngày, tháng, năm
+		// hàm nhập ngày, xuat
+		void inputDate();
 		friend istream& operator >> ( istream& is, Date &date );
-		// hàm xuất ngày tháng năm
 		friend ostream& operator << ( ostream& os, const Date &date );
 		// nạp chồng toán tử gán
 		Date operator = ( const Date &date );
@@ -38,6 +38,7 @@ class Date {
 		friend bool operator >= ( const Date &date1, const Date &date2 );
 		friend bool operator <= ( const Date &date1, const Date &date2 );
 		friend bool operator == ( const Date &date1, const Date &date2 );
+		friend int age( const Date &date );
 		// hàm lấy giá trị ngay, tháng , năm
 		int getDay();
 		int getMonth();
@@ -62,6 +63,7 @@ class Time {
 		friend bool operator > ( const Time &time1, const Time &time2 );
 		friend bool operator >= ( const Time &time1, const Time &time2 );
 		friend bool operator <= ( const Time &time1, const Time &time2 );
+		friend int operator - ( const Time &time1, const Time &time2 );
 		int getHour();
 		int getMin();
 };
@@ -82,6 +84,20 @@ Date::Date( const Date &date ){
 	this->month = date.month;
 	this->year = date.year;
 }
+int age(const Date &date) {
+    time_t t = time(0);
+    struct tm *now = localtime(&t);
+    // lấy ngày tháng năm hiện tại
+    int current_year = now->tm_year + 1900;
+    int current_month = now->tm_mon + 1;
+    int current_day = now->tm_mday;
+    int age = current_year - date.year;
+    // kiểm tra xem tới ngày hiện tại chưa
+    if (current_month < date.month || (current_month == date.month && current_day < date.day)) {
+        age--;
+    }
+    return age;
+}
 void inputDay( int &day, int maxDay ){ // Ham check input day
 	do {
 		cout << "Nhap ngay:  ";
@@ -90,31 +106,74 @@ void inputDay( int &day, int maxDay ){ // Ham check input day
 			cout << "\nNgay phai >= 1 va <= " << maxDay << ". Vui long nhap lai\n\n";
 	}while( day < 1 || day > maxDay );
 }
+void Date::inputDate(){
+    time_t t = time(nullptr); // L?y th?i gian hi?n t?i
+    tm* now = localtime(&t);
+    year = 2024;
+    do {
+        cout << "Nhap thang:  ";
+        cin >> month;
+        if( month < 1 || month > 12 ){
+            cout << "\nThang phai >= 1 va <= 12. Vui long nhap lai\n\n";
+            continue;
+        }
+        if( month < now->tm_mon+1 )
+            cout << "\nThang phai lon hon hoac bang thang hien tai. Vui long nhap lai\n\n";
+    }while( month < 1 || month > 12 || month < now->tm_mon+1 );
+    do {
+        if( month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12 )
+            inputDay(day,31);
+        else if( month == 4 || month == 6 || month == 9 || month == 11 )
+            inputDay(day,30);
+        else {
+            if( year % 400 == 0 || ( year % 4 == 0 && year % 100 != 0 ) ){
+                inputDay(day,29);
+                dayOfMonth[1] = 29;
+            }
+            else
+                inputDay(day,28);
+        }
+        if( day < now->tm_mday )
+            cout << "\nNgay phai lon hon hoac bang ngay hien tai\n\n";
+    }while( day < now->tm_mday );
+}
 istream& operator >> ( istream& is, Date &date ){
-	do {
-		cout << "Nhap nam:  ";
-		is >> date.year;
-		if( date.year < 1900 )
-			cout << "\nNam phai >= 1900. Vui long nhap lai\n\n";
-	}while( date.year < 1900 );
-	do {
-		cout << "Nhap thang:  ";
-		is >> date.month;
-		if( date.month < 1 || date.month > 12 )
-			cout << "\nThang phai >= 1 va <= 12. Vui long nhap lai\n\n";
-	}while( date.month < 1 || date.month > 12 );
-	if( date.month == 1 || date.month == 3 || date.month == 5 || date.month == 7 || date.month == 8 || date.month == 10 || date.month == 12 )
-		inputDay(date.day,31);
-	else if( date.month == 4 || date.month == 6 || date.month == 9 || date.month == 11 )
-		inputDay(date.day,30);
-	else {
-		if( date.year % 400 == 0 || ( date.year % 4 == 0 && date.year % 100 != 0 ) ){
-			inputDay(date.day,29);
-			date.dayOfMonth[1] = 29;
-		}
-		else
-			inputDay(date.day,28);
-	}
+    time_t t = time(nullptr);
+    tm* now = localtime(&t);
+    do {
+        do {
+            cout << "Nhap nam:  ";
+            is >> date.year;
+            if( date.year < 1900 ){
+                cout << "\nNam phai >= 1900. Vui long nhap lai\n\n";
+                continue;
+            }
+            if( date.year > now->tm_year+1900 ){
+                cout << "\nNam khong hop le. Vui long nhap lai.\n\n";
+                continue;
+            }
+            if( date.year > 2012 )
+                cout << "\nNguoi tu 12 tuoi tro len moi duoc di may bay mot minh. Vui long nhap lai.\n";
+        }while( date.year < 1900 || date.year > now->tm_year+1900 || date.year > 2012 );
+        do {
+            cout << "Nhap thang:  ";
+            is >> date.month;
+            if( date.month < 1 || date.month > 12 )
+                cout << "\nThang phai >= 1 va <= 12. Vui long nhap lai\n\n";
+        }while( date.month < 1 || date.month > 12 );
+        if( date.month == 1 || date.month == 3 || date.month == 5 || date.month == 7 || date.month == 8 || date.month == 10 || date.month == 12 )
+            inputDay(date.day,31);
+        else if( date.month == 4 || date.month == 6 || date.month == 9 || date.month == 11 )
+            inputDay(date.day,30);
+        else {
+            if( date.year % 400 == 0 || ( date.year % 4 == 0 && date.year % 100 != 0 ) ){
+                inputDay(date.day,29);
+                date.dayOfMonth[1] = 29;
+            }
+            else
+                inputDay(date.day,28);
+        }
+    }while( age(date) < 12 );
 	return is;
 }
 ostream& operator << ( ostream& os, const Date &date ){
@@ -191,6 +250,7 @@ int Date::getMonth(){
 int Date::getYear(){
 	return year;
 }
+
 
 
 // Build class Time
@@ -273,4 +333,9 @@ int Time::getHour(){
 }
 int Time::getMin(){
 	return minute;
+}
+int operator - ( const Time &time1, const Time &time2 ){
+    int h = time1.hour-time2.hour;
+    int m = time1.minute-time2.minute;
+    return h*60 + m;
 }
