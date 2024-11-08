7f29a259751284be638e503b53d99028ca49abe8
hoanglam0905 <hoanglam21092005@gmail.com>
2024-11-08 23:35:28 +0700

    update

---
+++ SourceCode/human.h
@@ -0,0 +1,15 @@
+#pragma once
+#include "date_time.h"
+#include "flight.h"
+#include "plane.h"
+class Human {
+	private:
+		string name;
+		Date date;
+		string sex; // giới tính
+		int age;
+	public:
+		
+};
+
+
