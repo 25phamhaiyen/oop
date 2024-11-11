1808789483b778a33589cd8f138e239aa8017a5f
hoanglam0905 <hoanglam21092005@gmail.com>
2024-11-11 23:31:56 +0700

    Update ticket.h

--- SourceCode/ticket.h
+++ SourceCode/ticket.h
@@ -21,3 +21,19 @@ Ticket::Ticket(string ticketID, string flightNumber, string passengerPassport,
       seatNumber(seatNumber), seatClass(seatClass) {
     price = calculatePrice();
 }
+double Ticket::calculatePrice() {
+    double basePrice = 100.0;
+
+    if (seatClass == "First Class") {
+        basePrice *= 3.0;
+    } else if (seatClass == "Business") {
+        basePrice *= 2.0;
+    } else if (seatClass == "Economy") {
+        basePrice *= 1.0;
+    }
+    return basePrice;
+}
+
+double Ticket::getPrice() const {
+    return price;
+}
