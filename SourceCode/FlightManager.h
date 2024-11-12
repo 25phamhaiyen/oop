745792f5237f43fefd4184472cca231d2452732c
hoanglam0905 <hoanglam21092005@gmail.com>
2024-11-12 23:44:32 +0700

    add file flightManager.h

---
+++ SourceCode/FlightManager.h
@@ -0,0 +1,12 @@
+class FlightManager {
+private:                  
+    vector<Flight> airports;               
+
+public:
+    void addFlight(const Flightt& flight);
+	//xoa, sua, tim kiem
+
+
+    void printMenu();
+};
+
